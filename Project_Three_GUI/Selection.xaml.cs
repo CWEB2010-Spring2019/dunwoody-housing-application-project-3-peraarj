eba1a244e37600df0a646540cf5b404e8c4fc9f6
peraarj <peraarj@dunwoody.edu>
2019-04-04 11:54:11 -0500

    updating ui and class structure

---
+++ Project_Three_GUI/Selection.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_Three_GUI
+{
+    /// <summary>
+    /// Interaction logic for Selection.xaml
+    /// </summary>
+    public partial class Selection : Window
+    {
+        public Selection()
+        {
+            InitializeComponent();
+        }
+
+		private void NewRecord_Btn_Click(object sender, RoutedEventArgs e)
+		{
+			ResidentEntry entry = new ResidentEntry();
+			entry.Show();
+			this.Close();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			ResidentSearch search = new ResidentSearch();
+			search.Show();
+			this.Close();
+		}
+	}
+}
---
+++ Project_Three_GUI/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Res_ID] NCHAR(5) NOT NULL PRIMARY KEY IDENTITY, 
+    [Res_FName] NCHAR(15) NOT NULL, 
+    [Res_LName] NCHAR(15) NOT NULL, 
+    [Res_Cat] NCHAR(10) NOT NULL, 
+    [Room_Num] INT NOT NULL, 
+    [Floor_Num] INT NOT NULL
+)
+
+GO
